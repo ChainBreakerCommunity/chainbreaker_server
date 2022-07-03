7f25d724b0ecee8b26b5322f9e01c562de814f42
Juanchobanano <juancepeda.gestion@gmail.com>
2022-07-02 21:05:15 -0500

    Logging functions

---
+++ logging/user.py
@@ -0,0 +1,14 @@
+import logging
+
+def get_user_logging():
+    return
+
+def get_graph_logging():
+    return 
+
+def get_data_logging():
+    return 
+
+def get_scraper_logging():
+    return 
+    
